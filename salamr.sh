3f5e364a7e752fe752dfd4b6a8c96e3f73fe3025
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2024-03-14 16:43:21 +0000

    Added record and play feature

--- salamr.sh
+++ salamr.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
 
-while getopts ":l:m:help" opt; do
+show_help() {
+  echo
+  echo -l \(Locate\) - Locate and Tap single item - Usage: ~\$salamr -l \"Home\"
+  echo
+  echo -m \(Multi Locate\) - Locate and Tap multiple item in a sequence - Usage: ~\$salamr -m \"Account,Theme,Dark\" \(avoid extra spaces\)
+  echo
+  echo -r \(Record\) - Record inputs on emulator - Usage: ~\$salamr -r
+  echo
+  echo -p \(Play\) - Play recorded inputs on emulator - Usage: ~\$salamr -p
+  echo
+  echo
+}
+
+while getopts ":l:m:hrp" opt; do
   case ${opt} in
-    h )
+    h)
       # -h option
-      echo
-      echo -l \(Locate\) - Locate and Tap single item. Usage:  $\salamr -l \"Home\"
-      echo
-      echo -m \(Multi Locate\) - Locate and Tap multiple item in a sequence. Usage $\salamr -m \"Account,Theme,Dark\" \(avoid extra spaces\)
-      echo
+      show_help
       ;;
-    l )
+    l)
       # -l option
-      script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"  
-      bash "$script_dir/bin/locator"  "$OPTARG"
+      script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+      bash "$script_dir/bin/locator" "$OPTARG"
       ;;
-    m )
+    m)
       # -m option
-      script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"      
+      script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
       bash "$script_dir/bin/locate_multiple" "$OPTARG"
       ;;
-    \? )
+    r)
+      # -r option
+      script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+      bash "$script_dir/bin/recorder"
+      ;;
+    p)
+      # -p option
+      script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+      bash "$script_dir/bin/replay"
+      ;;
+    \?)
       echo "Invalid option: $OPTARG" >&2
       ;;
-    : )
-      echo "Invalid option: $OPTARG requires an argument" >&2
+    :)
+      # This case won't occur since r and p don't take arguments
       ;;
   esac
 done
