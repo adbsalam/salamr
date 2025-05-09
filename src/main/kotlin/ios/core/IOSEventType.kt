fd0491b1411c09df876bea7c042d2dbeee2718e3
muhammad-abdulsalam <muhammad.abdulsalam@autotrader.co.uk>
2025-05-09 11:37:54 +0100

    added sample from ios code

--- src/main/kotlin/ios/core/IOSEventType.kt
+++ src/main/kotlin/ios/core/IOSEventType.kt
@@ -19,3 +19,65 @@ data class IOSEventType(
             }
     }
 }
+
+/**
+ * import ATSupport
+ * import os.log
+ * import UIKit
+ *
+ * final class ATWindow: UIWindow {
+ *     private let logger = os.Logger(
+ *         subsystem: Bundle.main.bundleIdentifier ?? "com.salamr",
+ *         category: "UserInputs")
+ *
+ *     override public func motionEnded(
+ *         _ motion: UIEvent.EventSubtype, with _: UIEvent?
+ *     ) {
+ *         if motion == .motionShake,
+ *             isDebug || AppConfig.shared.featureManager.isDeveloperModeEnabled
+ *         {
+ *             let topMostController =
+ *                 rootViewController?.presentedViewController
+ *                 ?? rootViewController
+ *
+ *             topMostController?.present(
+ *                 BaseNavigationController(
+ *                     rootViewController: HiddenMenuViewController()),
+ *                 animated: true
+ *             )
+ *
+ *         }
+ *     }
+ *
+ *     override public func sendEvent(_ event: UIEvent) {
+ *         // Get all touches from the event
+ *         let touches = event.allTouches
+ *
+ *         // Iterate over each touch
+ *         for touch in touches ?? [] {
+ *             let phase = touch.phase
+ *             let location = touch.location(in: touch.window)
+ *             let timestamp = event.timestamp
+ *
+ *             // Log Tap events (when phase is Began or Ended)
+ *             if phase == .began || phase == .ended {
+ *                 logger.info(
+ *                     "salamr-- Tap Event - Timestamp: \(timestamp), Location: \(location.x), \(location.y), Phase: \(phase.rawValue)"
+ *                 )
+ *             }
+ *
+ *             // Log Swipe events (when phase is Moved)
+ *             if phase == .moved {
+ *                 logger.info(
+ *                     "Swipe Event - Timestamp: \(timestamp), Location: \(location.x), \(location.y), Phase: \(phase.rawValue)"
+ *                 )
+ *             }
+ *         }
+ *
+ *         // Call the super implementation to ensure the event is passed to the system
+ *         super.sendEvent(event)
+ *
+ *     }
+ * }
+ *
+ */
